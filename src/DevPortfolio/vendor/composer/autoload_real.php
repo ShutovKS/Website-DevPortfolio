849904322c8af855d1f10dda0984a394cc64053e
ShutovKS <schutow.kir@yandex.ru>
2024-07-12 13:27:06 +0300

    Add symfony/var-dumper

--- src/DevPortfolio/vendor/composer/autoload_real.php
+++ src/DevPortfolio/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInitbf33068c08aa4bb5132da394f6b7043d
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitbf33068c08aa4bb5132da394f6b7043d::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
