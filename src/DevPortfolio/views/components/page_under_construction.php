050d7ec3406aae3ea333015aed84c735d30264e2
ShutovKS <schutow.kir@yandex.ru>
2024-07-13 23:49:15 +0300

    Add component "page_under_construction"

---
+++ src/DevPortfolio/views/components/page_under_construction.php
@@ -0,0 +1,11 @@
+<div class="text-center">
+
+    <!-- Страница находиться в разработке -->
+    <h2 class="h1 fw-bold mb-3">Page is under construction</h2>
+
+    <h3 class="h3 fw-medium mb-2">We are working on it. Please come back later.</h3>
+
+    <img src="https://cdn4.iconfinder.com/data/icons/under-construction-1/512/under-1024.png" class="img-fluid"
+         alt="Page is under construction">
+
+</div>
