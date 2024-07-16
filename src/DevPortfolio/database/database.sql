00a43522a8f695822a65772cc53891ea73434e36
ShutovKS <schutow.kir@yandex.ru>
2024-07-16 22:16:09 +0300

    Add articles table for db

--- src/DevPortfolio/database/database.sql
+++ src/DevPortfolio/database/database.sql
@@ -38,6 +38,7 @@ CREATE TABLE `users`
 --
 -- Данные таблицы `users`
 --
+
 INSERT INTO `users` (`username`, `full_name`, `link_to_photo`, `email`, `phone`, `job`,
                      `location_city`, `location_country`, `social_website`, `social_github`,
                      `social_vk`, `social_telegram`, `password_hash`, `salt`, `created_at`,
@@ -63,3 +64,32 @@ VALUES ('jdoe', 'John Doe',
         NOW(), 1, 1);
 
 -- Пароль для всех пользователей: 123456
+
+-- --------------------------------------------------------
+
+--
+-- Таблица `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE `articles`
+(
+    `id`         INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id`    INT(11) UNSIGNED NOT NULL,
+    `title`      VARCHAR(255) NOT NULL,
+    `content`    TEXT         NOT NULL,
+    `created_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `published`  TINYINT(1) NOT NULL DEFAULT '0',
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Данные таблицы `articles`
+--
+
+INSERT INTO `articles` (`user_id`, `title`, `content`, `created_at`, `updated_at`, `published`)
+VALUES (1, 'First Article by John Doe', 'This is the content of the first article by John Doe.', NOW(), NOW(), 1),
+       (2, 'First Article by Alice Smith', 'This is the content of the first article by Alice Smith.', NOW(), NOW(), 1),
+       (3, 'First Article by Bob Thompson', 'This is the content of the first article by Bob Thompson.', NOW(), NOW(), 1);
+
