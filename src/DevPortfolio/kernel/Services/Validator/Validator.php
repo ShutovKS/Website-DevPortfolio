d344b956e82c3e090ceaa0280097ac0f95874267
ShutovKS <schutow.kir@yandex.ru>
2024-07-19 18:06:28 +0300

    Added checks to the validator

--- src/DevPortfolio/kernel/Services/Validator/Validator.php
+++ src/DevPortfolio/kernel/Services/Validator/Validator.php
@@ -65,9 +65,63 @@ class Validator implements ValidatorInterface
                     return 'Field ' . $key . ' must be a reachable URL';
                 }
                 break;
+            case 'no_scripts':
+                if ($this->containsScripts($value)) {
+                    return 'Field ' . $key . ' must not contain HTML or script content';
+                }
+                break;
+            case 'numeric':
+                if (!is_numeric($value)) {
+                    return 'Field ' . $key . ' must be a numeric value';
+                }
+                break;
+            case 'integer':
+                if (!filter_var($value, FILTER_VALIDATE_INT)) {
+                    return 'Field ' . $key . ' must be an integer';
+                }
+                break;
+            case 'url':
+                if (!filter_var($value, FILTER_VALIDATE_URL)) {
+                    return 'Field ' . $key . ' must be a valid URL';
+                }
+                break;
+            case 'in':
+                $allowedValues = explode(',', $ruleValue);
+                if (!in_array($value, $allowedValues, true)) {
+                    return 'Field ' . $key . ' must be one of the following values: ' . implode(', ', $allowedValues);
+                }
+                break;
+            case 'date':
+                if (!strtotime($value)) {
+                    return 'Field ' . $key . ' must be a valid date';
+                }
+                break;
+            case 'length':
+                if (strlen($value) !== $ruleValue) {
+                    return 'Field ' . $key . ' must be exactly ' . $ruleValue . ' characters';
+                }
+                break;
+            case 'ip':
+                if (!filter_var($value, FILTER_VALIDATE_IP)) {
+                    return 'Field ' . $key . ' must be a valid IP address';
+                }
+                break;
+            case 'between':
+                [$min, $max] = explode(',', $ruleValue);
+                if ($value < $min || $value > $max) {
+                    return 'Field ' . $key . ' must be between ' . $min . ' and ' . $max;
+                }
+                break;
         }
 
         return null;
+    }
+
+    private function containsScripts(string $value): bool
+    {
+        // Regular expression to detect HTML tags or JavaScript
+        $pattern = '/<[^>]*script.*?>.*?<\/[^>]*script.*?>|<[^>]+>|&lt;[^&]+&gt;|<script.*?>.*?<\/script.*?>/i';
+        return preg_match($pattern, $value) === 1;
     }
 }
 
